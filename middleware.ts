6755b2152ecf5e00c4518c3df9c9ac58406c7f21
Valar <azgrhl@gmail.com>
2024-11-15 19:58:34 +0800

    sitemap

--- middleware.ts
+++ middleware.ts
@@ -7,7 +7,7 @@ acceptLanguage.languages(languages);
 
 export const config = {
   // matcher: '/:lng*'
-  matcher: ['/((?!common|api|_next/static|_next/image|favicon.ico|favicon.svg).*)'],
+  matcher: ['/((?!common|api|_next/static|_next/image|favicon.ico|favicon.svg|sitemap\.xml).*)'],
   // matcher: ["/(en|zh-hant)/:path*"],
 };
 
