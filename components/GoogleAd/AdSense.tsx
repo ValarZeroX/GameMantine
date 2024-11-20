c5277240c3f31b8c0c7a7c8e31921ce05874db1f
Valar <azgrhl@gmail.com>
2024-11-20 11:30:59 +0800

    google 廣告

--- components/GoogleAd/AdSense.tsx
+++ components/GoogleAd/AdSense.tsx
@@ -1,17 +1,16 @@
 import Script from 'next/script';
-import React from 'react'
 
 type AdsenseTypes = {
     pId: string;
 }
 
-const AdSense = ({ pId }: AdsenseTypes) => {
+const AdSense : React.FC<AdsenseTypes> = ({ pId }) => {
   return (
     <Script
         async
         src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-${pId}`}
         crossOrigin='anonymous'
-        strategy='afterInteractive'
+        strategy="afterInteractive"
     />
   )
 }
