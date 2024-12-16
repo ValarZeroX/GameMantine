9f40f79f97a5d4f514f2cfba4236353909a8cfa8
Valar <azgrhl@gmail.com>
2024-12-16 16:22:05 +0800

    next

---
+++ .next/server/_ssr_app_i18n_locales_en_ability_json.js
@@ -0,0 +1,25 @@
+"use strict";
+/*
+ * ATTENTION: An "eval-source-map" devtool has been used.
+ * This devtool is neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file with attached SourceMaps in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+exports.id = "_ssr_app_i18n_locales_en_ability_json";
+exports.ids = ["_ssr_app_i18n_locales_en_ability_json"];
+exports.modules = {
+
+/***/ "(ssr)/./app/i18n/locales/en/ability.json":
+/*!******************************************!*\
+  !*** ./app/i18n/locales/en/ability.json ***!
+  \******************************************/
+/***/ ((module) => {
+
+module.exports = /*#__PURE__*/JSON.parse('{"粉療":{"name":"Powder Heal","description":"Once during your turn, you may heal 20 damage from each of your Pokémon."},"反擊":{"name":"Counterattack","description":"If this Pokémon is in the Active Spot and is damaged by an attack from your opponent\'s Pokémon, do 20 damage to the attacking Pokémon."},"硬殼盔甲":{"name":"Shell Armor","description":"This Pokémon takes -10 damage from attacks."},"芳香陷阱":{"name":"Fragrance Trap","description":"If this Pokémon is in the Active Spot, once during your turn, you may switch in 1 of your opponent\'s Benched Basic Pokémon to the Active Spot."},"水手裡劍":{"name":"Water Shuriken","description":"Once during your turn, you may do 20 damage to 1 of your opponent\'s Pokémon."},"伏特充能":{"name":"Volt Charge","description":"Once during your turn, you may take a Lightning Energy from your Energy Zone and attach it to this Pokémon."},"影子咒縛":{"name":"Shadowy Spellbind","description":"As long as this Pokémon is in the Active Spot, your opponent can\'t use any Supporter cards from their hand."},"引夢擺錘":{"name":"Sleep Pendulum","description":"Once during your turn, you may flip a coin. If heads, your opponent\'s Active Pokémon is now Asleep."},"移形換影":{"name":"Psy Shadow","description":"Once during your turn, you may take 1 Psychic Energy from your Energy Zone and attach it to the Psychic Pokémon in the Active Spot."},"瓦斯漏氣":{"name":"Gas Leak","description":"Once during your turn, if this Pokémon is in the Active Spot, you may make your opponent\'s Active Pokémon Poisoned."},"堅硬膜":{"name":"Hard Coat","description":"This Pokémon takes -20 damage from attacks."},"驅離":{"name":"Wing Attack","description":"Once during your turn, you may switch out your opponent\'s Active Pokémon to the Bench. (Your opponent chooses the new Active Pokémon.)"},"資料掃描":{"name":"Data Scan","description":"Once during your turn, you may look at the top card of your deck."}}');
+
+/***/ })
+
+};
+;
